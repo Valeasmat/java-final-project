7294dfdaf17d226da3c5b23c5ec6d026bc08b781
valeria <valeriaasmat2dob@gmail.com>
2022-02-01 20:39:34 -0500

    Exception creation UnniversityCommunityMember class creation

---
+++ src/main/java/com/globant/university/exception/InvalidScannerInputException.java
@@ -0,0 +1,7 @@
+package com.globant.university.exception;
+
+public class InvalidScannerInputException extends Exception{
+    public InvalidScannerInputException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/globant/university/peopleDataObject/UniversityCommunityMember.java
@@ -0,0 +1,9 @@
+package com.globant.university.peopleDataObject;
+
+public abstract class UniversityCommunityMember {
+    private String name;
+    private String id;
+    private int age;
+    private static int counter=1;
+
+}
